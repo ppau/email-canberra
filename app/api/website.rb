54b923862a01ba71c8a8beae46ad72905c6ecf46
Sag0Sag0 <laurencestevens@fvfsn0tfh3qf.gateway>
2017-02-06 19:46:14 +1100

    Update on website

--- app/api/website.rb
+++ app/api/website.rb
@@ -2,3 +2,40 @@ require "sinatra"
 
 $path = File.join('public', 'db/*.toml')
 $array = Dir[$path]
+
+def get_website(path)
+  website = false
+
+  IO.foreach(path) do |line|
+    if line =~ /website =/
+      website = line.gsub(/website =/, '')
+    end
+  end
+
+  if website != false
+    return website
+  end
+end
+
+def parse_attribute_n(path, regular_str)
+  output = []
+
+  IO.foreach(path) do |line|
+    if line =~ /#{regular_str}/
+      output.push(get_name(path))
+    end
+  end
+
+  if output != nil or []
+    return output
+  end
+
+end
+
+get '/api/website' do
+  member_websites = []
+  $array.each do |item|
+    member_websites.push(get_website(item))
+  end
+  member_websites.to_json
+end
